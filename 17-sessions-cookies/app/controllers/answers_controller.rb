d30b54f7d26f305fed84f3b1885b4064b7540846
Sam Barker <samuel.e.barker@googlemail.com>
2019-07-04 18:08:53 +0100

    added 17 again

---
+++ 17-sessions-cookies/app/controllers/answers_controller.rb
@@ -0,0 +1,32 @@
+class AnswersController < ApplicationController
+
+    def new
+        @user = session[:user_name] ? User.find_by(name: session[:user_name]) : User.new
+        @instructor = Instructor.all.sample
+        @answer = Answer.new
+        @options = Cookie.options(@instructor.cookie)
+        session[:score] ||=  0
+    end
+    
+    def create
+        question = Question.create({instructor_id: params[:answer][:instructor_id]})
+        answer = Answer.create(answer_params)
+        answer.question = question
+        if answer.valid? && answer.user.valid?
+            session[:user_name] = answer.user.name
+            session[:score] += answer.correct ? 1 : 0
+            answer.save
+            flash[:notice] = answer.correct ? 'Well done!' : 'Wrong!'
+            redirect_to new_answer_path
+        else
+            flash[:errors] = answer.errors.full_messages + answer.user.errors.full_messages
+            redirect_to new_answer_path
+        end
+    end
+
+    private
+
+    def answer_params
+        params.require(:answer).permit(:cookie_id, :user_id, :user_name)
+    end
+end
